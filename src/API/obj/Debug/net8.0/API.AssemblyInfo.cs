45fc1d07bcf9f708bb0a7339d177d6320b28f6e3
Matheus <matheusmenezesribeiro@hotmail.com>
2024-07-15 20:34:12 -0300

    fix

--- src/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ src/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33bf4be1db7c70249e6433140d4d2975e0ad1772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+677eefeafce666f3a4c5e7686ad713ebff814fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/DOMAIN/obj/Debug/net8.0/DOMAIN.AssemblyInfo.cs
+++ src/DOMAIN/obj/Debug/net8.0/DOMAIN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DOMAIN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33bf4be1db7c70249e6433140d4d2975e0ad1772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+677eefeafce666f3a4c5e7686ad713ebff814fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DOMAIN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DOMAIN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
